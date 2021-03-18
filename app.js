068182ec0424ba1c228ff7fe4957d2fc7b262832
tatiana84 <tantamuntean2018@gmail.com>
2021-03-18 22:48:10 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let myAge = 36; // this is a variable with my age
+
+let earlyYears = 2 * 10.5; // earlyYears is 2*10.5
+
+let laterYears = myAge / 2; // laterYears is myAge / 2
+
+let numberDogYears = laterYears * 4; // numberDogYears is laterYears * 4
+
+//console.log(earlyYears);
+
+//console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears; // myAgeInDogYears is earlyYears + laterYears
+
+//console.log(myAgeInDogYears);
+
+let myName = ('Tatiana'.toLowerCase()); // a variable declared with my name
+
+//console.log(myName.toLowerCase());
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
